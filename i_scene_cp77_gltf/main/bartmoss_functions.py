7bab6da5d35f86f3b0f0bc8b43eb1d9232eef9da
Simarilius <simarilius@yahoo.com>
2023-12-18 23:20:42 +0000

    merge bartmoss

--- i_scene_cp77_gltf/main/bartmoss_functions.py
+++ i_scene_cp77_gltf/main/bartmoss_functions.py
@@ -36,6 +36,201 @@ def calculate_mesh_volume(obj):
     bpy.ops.rigidbody.mass_calculate(material='Custom', density=1) # density in kg/m^3
     volume = obj.rigid_body.mass
     return volume
+
+def checkexists(meshname, Masters):
+    groupname = os.path.splitext(os.path.split(meshname['$value'])[-1])[0]
+    if groupname in Masters.children.keys() and len(Masters.children[groupname].objects)>0:
+        return True
+    else:
+        return False
+
+def get_pos(inst):
+    pos=[0,0,0]
+    if 'Position' in inst.keys():
+        if 'Properties' in inst['Position'].keys():
+            pos[0] = inst['Position']['Properties']['X'] /100
+            pos[1] = inst['Position']['Properties']['Y'] /100
+            pos[2] = inst['Position']['Properties']['Z'] /100          
+        else:
+            if 'X' in inst['Position'].keys():
+                pos[0] = inst['Position']['X'] /100
+                pos[1] = inst['Position']['Y'] /100
+                pos[2] = inst['Position']['Z'] /100
+            else:
+                pos[0] = inst['Position']['x'] /100
+                pos[1] = inst['Position']['y'] /100
+                pos[2] = inst['Position']['z'] /100
+    elif 'position' in inst.keys():
+        if 'X' in inst['position'].keys():
+                pos[0] = inst['position']['X'] /100
+                pos[1] = inst['position']['Y'] /100
+                pos[2] = inst['position']['Z'] /100
+    elif 'translation' in inst.keys():
+        pos[0] = inst['translation']['X'] /100
+        pos[1] = inst['translation']['Y'] /100
+        pos[2] = inst['translation']['Z'] /100
+    return pos
+
+def get_rot(inst):
+    rot=[0,0,0,0]
+    if 'Orientation' in inst.keys():
+        if 'Properties' in inst['Orientation'].keys():
+            rot[0] = inst['Orientation']['Properties']['r']  
+            rot[1] = inst['Orientation']['Properties']['i'] 
+            rot[2] = inst['Orientation']['Properties']['j'] 
+            rot[3] = inst['Orientation']['Properties']['k']            
+        else:
+            rot[0] = inst['Orientation']['r'] 
+            rot[1] = inst['Orientation']['i'] 
+            rot[2] = inst['Orientation']['j'] 
+            rot[3] = inst['Orientation']['k'] 
+    elif 'orientation' in inst.keys():
+            rot[0] = inst['orientation']['r'] 
+            rot[1] = inst['orientation']['i'] 
+            rot[2] = inst['orientation']['j'] 
+            rot[3] = inst['orientation']['k'] 
+    elif 'Rotation' in inst.keys():
+            rot[0] = inst['Rotation']['r'] 
+            rot[1] = inst['Rotation']['i'] 
+            rot[2] = inst['Rotation']['j'] 
+            rot[3] = inst['Rotation']['k'] 
+    elif 'rotation' in inst.keys():
+            rot[0] = inst['rotation']['r'] 
+            rot[1] = inst['rotation']['i'] 
+            rot[2] = inst['rotation']['j'] 
+            rot[3] = inst['rotation']['k'] 
+    return rot
+
+def set_pos(inst,obj):  
+    #print(inst)  
+    if 'Position'in inst.keys():
+        if 'Properties' in inst['Position'].keys():
+            inst['Position']['Properties']['X']= float("{:.9g}".format(obj.location[0]*100))
+            inst['Position']['Properties']['Y'] = float("{:.9g}".format(obj.location[1]*100))
+            inst['Position']['Properties']['Z'] = float("{:.9g}".format(obj.location[2]*100))
+        else:
+            if 'X' in inst['Position'].keys():
+                inst['Position']['X'] = float("{:.9g}".format(obj.location[0]*100))
+                inst['Position']['Y'] = float("{:.9g}".format(obj.location[1]*100))
+                inst['Position']['Z'] = float("{:.9g}".format(obj.location[2]*100))
+            else:
+                inst['Position']['x'] = float("{:.9g}".format(obj.location[0]*100))
+                inst['Position']['y'] = float("{:.9g}".format(obj.location[1]*100))
+                inst['Position']['z'] = float("{:.9g}".format(obj.location[2]*100))
+    elif 'position' in inst.keys():
+        inst['position']['X'] = float("{:.9g}".format(obj.location[0]*100))
+        inst['position']['Y'] = float("{:.9g}".format(obj.location[1]*100))
+        inst['position']['Z'] = float("{:.9g}".format(obj.location[2]*100))
+    elif 'translation' in inst.keys():
+        inst['translation']['X'] = float("{:.9g}".format(obj.location[0]*100))
+        inst['translation']['Y'] = float("{:.9g}".format(obj.location[1]*100))
+        inst['translation']['Z'] = float("{:.9g}".format(obj.location[2]*100))
+
+def set_z_pos(inst,obj):  
+    #print(inst)  
+    if 'Position'in inst.keys():
+        if 'Properties' in inst['Position'].keys():
+            inst['Position']['Properties']['Z'] = float("{:.9g}".format(obj.location[2]*100))
+        else:
+            if 'X' in inst['Position'].keys():
+                inst['Position']['Z'] = float("{:.9g}".format(obj.location[2]*100))
+            else:
+                inst['Position']['z'] = float("{:.9g}".format(obj.location[2]*100))
+    elif 'position' in inst.keys():
+        inst['position']['Z'] = float("{:.9g}".format(obj.location[2]*100))
+    elif 'translation' in inst.keys():
+        inst['translation']['Z'] = float("{:.9g}".format(obj.location[2]*100))
+
+def set_rot(inst,obj):
+    if 'Orientation' in inst.keys():
+        if 'Properties' in inst['Orientation'].keys():
+            inst['Orientation']['Properties']['r'] = float("{:.9g}".format(obj.rotation_quaternion[0] ))
+            inst['Orientation']['Properties']['i'] = float("{:.9g}".format(obj.rotation_quaternion[1] )) 
+            inst['Orientation']['Properties']['j'] = float("{:.9g}".format(obj.rotation_quaternion[2] ))  
+            inst['Orientation']['Properties']['k'] = float("{:.9g}".format(obj.rotation_quaternion[3] ))        
+        else:
+            inst['Orientation']['r'] = float("{:.9g}".format(obj.rotation_quaternion[0] ))
+            inst['Orientation']['i'] = float("{:.9g}".format(obj.rotation_quaternion[1] ))
+            inst['Orientation']['j'] = float("{:.9g}".format(obj.rotation_quaternion[2] ))
+            inst['Orientation']['k'] = float("{:.9g}".format(obj.rotation_quaternion[3] ))
+    elif 'Rotation' in inst.keys():
+            inst['Rotation']['r'] = float("{:.9g}".format(obj.rotation_quaternion[0] ))
+            inst['Rotation']['i'] = float("{:.9g}".format(obj.rotation_quaternion[1] )) 
+            inst['Rotation']['j'] = float("{:.9g}".format(obj.rotation_quaternion[2] ))
+            inst['Rotation']['k'] = float("{:.9g}".format(obj.rotation_quaternion[3] ))
+    elif 'rotation' in inst.keys():
+            inst['rotation']['r'] = float("{:.9g}".format(obj.rotation_quaternion[0] ))
+            inst['rotation']['i'] = float("{:.9g}".format(obj.rotation_quaternion[1] )) 
+            inst['rotation']['j'] = float("{:.9g}".format(obj.rotation_quaternion[2] ))
+            inst['rotation']['k'] = float("{:.9g}".format(obj.rotation_quaternion[3] ))
+    elif 'orientation' in inst.keys():
+            inst['orientation']['r'] = float("{:.9g}".format(obj.rotation_quaternion[0] ))
+            inst['orientation']['i'] = float("{:.9g}".format(obj.rotation_quaternion[1] )) 
+            inst['orientation']['j'] = float("{:.9g}".format(obj.rotation_quaternion[2] ))
+            inst['orientation']['k'] = float("{:.9g}".format(obj.rotation_quaternion[3] ))
+
+def set_scale(inst,obj):
+    if 'Scale' in inst.keys():
+        if 'Properties' in inst['Scale'].keys():
+            inst['Scale']['Properties']['X'] = float("{:.9g}".format(obj.scale[0]*100))
+            inst['Scale']['Properties']['Y'] = float("{:.9g}".format(obj.scale[1]*100))
+            inst['Scale']['Properties']['Z']= float("{:.9g}".format(obj.scale[2]*100))
+        else:
+            inst['Scale']['X']  = float("{:.9g}".format(obj.scale[0]*100))
+            inst['Scale']['Y']  = float("{:.9g}".format(obj.scale[1]*100))
+            inst['Scale']['Z']  = float("{:.9g}".format(obj.scale[2]*100))
+    elif 'scale' in inst.keys():
+            inst['scale']['X']  = float("{:.9g}".format(obj.scale[0]*100))
+            inst['scale']['Y']  = float("{:.9g}".format(obj.scale[1]*100))
+            inst['scale']['Z']  = float("{:.9g}".format(obj.scale[2]*100))
+
+def set_bounds(node, obj):
+        node["Bounds"]['Max']["X"]= float("{:.9g}".format(obj.location[0]*100))
+        node["Bounds"]['Max']["Y"]= float("{:.9g}".format(obj.location[1]*100))
+        node["Bounds"]['Max']["Z"]= float("{:.9g}".format(obj.location[2]*100))
+        node["Bounds"]['Min']["X"]= float("{:.9g}".format(obj.location[0]*100))
+        node["Bounds"]['Min']["Y"]= float("{:.9g}".format(obj.location[1]*100))
+        node["Bounds"]['Min']["Z"]= float("{:.9g}".format(obj.location[2]*100))
+
+def find_col(NodeIndex,Inst_idx,Sector_coll):
+    #print('Looking for NodeIndex ',NodeIndex,' Inst_idx ',Inst_idx, ' in ',Sector_coll)
+    col=[x for x in Sector_coll.children if x['nodeIndex']==NodeIndex]
+    if len(col)==0:
+        return None
+    elif len(col)==1:
+        return col[0]
+    else: 
+        inst=[x for x in col if x['instance_idx']==Inst_idx]
+        if len(inst)>0:
+            return inst[0]
+    return None
+
+def find_wIDMN_col(NodeIndex,tl_inst_idx, sub_inst_idx,Sector_coll):
+    #print('Looking for NodeIndex ',NodeIndex,' Inst_idx ',Inst_idx, ' in ',Sector_coll)
+    col=[x for x in Sector_coll.children if x['nodeIndex']==NodeIndex]
+    if len(col)==0:
+        return None
+    elif len(col)==1:
+        return col[0]
+    else: 
+        inst=[x for x in col if x['tl_instance_idx']==tl_inst_idx and x['sub_instance_idx']==sub_inst_idx]
+        if len(inst)>0:
+            return inst[0]
+    return None
+
+def find_decal(NodeIndex,Inst_idx,Sector_coll):
+    #print('Looking for NodeIndex ',NodeIndex,' Inst_idx ',Inst_idx, ' in ',Sector_coll)
+    col=[x for x in Sector_coll.objects if x['nodeIndex']==NodeIndex]
+    if len(col)==0:
+        return None
+    elif len(col)==1:
+        return col[0]
+    else: 
+        inst=[x for x in col if x['instance_idx']==Inst_idx]
+        if len(inst)>0:
+            return inst[0]
+    return None
+    return volume
     
 ## Returns True if the given object has shape keys, works for meshes and curves
 def hasShapeKeys(obj):
